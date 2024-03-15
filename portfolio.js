760a5fb29e4ed11ccb27cc797b4b234cbc7c4115
Christopher Anderson <CAnderson@us.ci.org>
2024-03-15 12:34:11 -0400

    Part Interacivty added

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,18 @@
-console.log('Hi welcome')
+let view = document.getElementById('tech')
+let closed = document.getElementById('cloz')
+
+let open = function() {
+    tech.style.display = 'none';
+    techs.style.display = 'block';
+    cloz.styles.display = 'block';
+}
+
+let close = function(){
+    tech.style.display = 'block';
+    techs.style.display = 'none';
+    cloz.styles.display = 'none';
+}
+
+view.addEventListener('click', open)
+
+closed.addEventListener('click', close)
